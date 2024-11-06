6a93ecbef1e86b1b6ee85ec7c345e9608499d937
abdunnoorfaheem <abdunnoor3030@gmail.com>
2024-11-06 07:55:59 +0600

    top section added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+// import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <section id="top-section">
+<div className="top-section-left">
+  <ul>
+  <li>FAQs</li>
+  <li>Support</li>
+  <li>Privacy</li>
+  <li>Policy</li>
+  <li id="career">Career</li>
+</ul>
+</div>
+<div className="top-section-right">
+  <p>info@example.com</p>
+  <p>+012 345 6789</p>
+</div>
+    </section>
+    // <section></section>
+    // <section></section>
+  )
+}
