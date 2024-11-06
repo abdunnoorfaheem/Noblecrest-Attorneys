5e0b2adc6ae32507d175747d8f3100483597b4d6
abdunnoorfaheem <abdunnoor3030@gmail.com>
2024-11-06 09:11:58 +0600

    banner section added

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,6 +2,15 @@
 
 export const Banner = () => {
   return (
-   <section></section>
+   <section className="banner">
+    <div className="banner-container">
+      <p>Business Consultancy</p>
+      <h1>Take Our Help To Reach <br /> The Top Level</h1>
+      <div className="banner-container-btn">
+        <button id="banner-btn1">Get Quote</button>
+        <button id="banner-btn2">Contact Us</button>
+      </div>
+    </div>
+   </section>
   )
 }
